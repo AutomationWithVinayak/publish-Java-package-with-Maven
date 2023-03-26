5ddda3f0134a389765aaec3c5f510c9b33e183ff
bhandurgevinayak <109057282+TestEngineer-I@users.noreply.github.com>
2023-03-26 11:57:46 +0530

    FA

---
+++ src/test/java/Demo1/Test1.java
@@ -0,0 +1,26 @@
+package Demo1;
+
+public class Test1 {
+
+public static void main(String[] args) {
+		
+		
+		
+		String a="0123456789";
+		String b ="";
+		
+		
+		for(int i=a.length()-1;  i>=0;  i--) {
+			
+			b=b+a.charAt(i);
+
+		}
+		System.out.println(b);
+		
+		
+		
+		
+		
+		
+	}
+}
